ba89c63cf27d98af49910a374ecc7df5e9ff43f8
Swapnil Bhagat <swapnil.bhagat@talentica.com>
2016-01-30 13:49:11 +0530

    added sample rest method

--- src/main/java/com/talcarfts/service/ProductService.java
+++ src/main/java/com/talcarfts/service/ProductService.java
@@ -1,8 +1,16 @@
 package com.talcarfts.service;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductService {
 
+	@RequestMapping(name = "/test")
+	public String test(@RequestParam(name = "test") String test) {
+		// to test use 
+		// http://localhost:8080/test?test=test
+		return test + 1;
+	}
 }
