b18464964121155421187cab9bb8ebdb3cbd6f64
Ashish Waghmare <ashish.waghmare@talentica.com>
2016-01-30 13:34:28 +0530

    initial spring starter checkin

---
+++ README.md
@@ -0,0 +1,1 @@
+Basic risk advisior Product and User
---
+++ src/main/java/com/talcarfts/RiskAdvisorApplication.java
@@ -0,0 +1,12 @@
+package com.talcarfts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskAdvisorApplication.class, args);
+	}
+}
